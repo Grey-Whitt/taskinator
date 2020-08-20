ef27ca12d184cad857d2281e7b4ff0d44a50b5e3
greyw <greywhitt@gmail.com>
2020-08-20 02:04:24 -0500

    sections now light up when task is over a droppable section

--- assets/js/script.js
+++ assets/js/script.js
@@ -201,7 +201,7 @@ var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
         event.preventDefault();
-        
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
 };
 
@@ -222,8 +222,18 @@ var dropTaskHandler = function(event) {
         statusSelectEl.selectedIndex = 2;
     };
 
+    dropZoneEl.removeAttribute("style");
+
     dropZoneEl.appendChild(draggableElement);
 };
+
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
+}
+
 
 
 pageContentEl.addEventListener('click', taskButtonHandler);
@@ -235,5 +245,8 @@ pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener('dragover', dropZoneDragHandler);
 
 pageContentEl.addEventListener("drop", dropTaskHandler);
+
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
+
 
 
